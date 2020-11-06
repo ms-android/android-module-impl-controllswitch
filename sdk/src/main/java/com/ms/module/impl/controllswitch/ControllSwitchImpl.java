560805a9eb5e258787fbd9ee8b4645c8e1705244
m_maohuawei <maohuawei@bdlbsc.com>
2020-11-06 19:25:31 +0800

    更新实现

--- sdk/src/main/java/com/ms/module/impl/controllswitch/ControllSwitchImpl.java
+++ sdk/src/main/java/com/ms/module/impl/controllswitch/ControllSwitchImpl.java
@@ -4,43 +4,58 @@ import com.ms.module.supers.inter.controll.IControllSwitchAdapter;
 
 public class ControllSwitchImpl extends IControllSwitchAdapter {
 
+    public ControllSwitchImpl() {
+        super();
+    }
 
-    private boolean logout;
+    private boolean logoutFlag;
+
+    private boolean tencentBuglyFlag = false;
+
+    private boolean aliyunLogFlag = false;
+
+
+    private boolean stackFlag;
 
     @Override
     public void setLogOut(boolean flag) {
-        logout = flag;
+        logoutFlag = flag;
     }
 
     @Override
     public boolean getLogOut() {
-        return logout;
+        return logoutFlag;
     }
 
 
-    private boolean stack;
-
     @Override
     public void setPrintStackTrace(boolean flag) {
-        stack = flag;
+        stackFlag = flag;
     }
 
     @Override
     public boolean getPrintStackTrace() {
-        return stack;
-    }
-
-
-    boolean requestlog = false;
-
-    @Override
-    public void setRequestLog(boolean flag) {
-        requestlog = flag;
+        return stackFlag;
     }
 
     @Override
-    public boolean getRequestLog() {
-        return requestlog;
+    public void setAliyunLogSend(boolean flag) {
+        aliyunLogFlag = flag;
+    }
+
+    @Override
+    public boolean getAliyunLogSend() {
+        return aliyunLogFlag;
+    }
+
+    @Override
+    public void setTencentBugly(boolean flag) {
+        tencentBuglyFlag = flag;
+    }
+
+    @Override
+    public boolean getTencentBugly() {
+        return tencentBuglyFlag;
     }
 }
 
